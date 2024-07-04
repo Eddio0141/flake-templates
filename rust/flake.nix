63d10eff1baf11bce00adb41508b151af36a32e9
Eddio0141 <eddio0141@gmail.com>
2024-07-04 21:22:27 +0000

    use oxalica/rust-overlay for rust bin instead

--- rust/flake.nix
+++ rust/flake.nix
@@ -4,41 +4,42 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { flake-parts, ... } @ inputs:
+  outputs = { flake-parts, nixpkgs, rust-overlay, ... } @ inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
       ];
       perSystem = { self', inputs', system, pkgs, ... }:
-      let
-        # check https://github.com/nix-community/fenix for what toolchains are available
-        toolchain = inputs'.fenix.packages.complete;
+        let
+          rust-doc = pkgs.writeShellApplication {
+            name = "rust-doc";
+            text = ''
+              xdg-open "${inputs'.fenix.packages.stable.rust-docs}/share/doc/rust/html/index.html"
+            '';
+          };
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (import rust-overlay)
+            ];
+          };
 
-        fenix = toolchain.withComponents [
-          "rustc"
-          "cargo"
-          "rustfmt"
-          "rust-analyzer"
-          "clippy"
-          "rust-src"
-        ];
-
-        rust-doc = pkgs.writeShellApplication {
-          name = "rust-doc";
-          text = ''
-            xdg-open "${toolchain.rust-docs}/share/doc/rust/html/index.html"
-          '';
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              rust-bin.stable.latest.default
+              rust-doc
+            ];
+          };
         };
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            fenix rust-doc
-          ];
-        };
-      };
     };
 }
