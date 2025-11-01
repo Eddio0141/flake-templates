260b9b09d5cd05e00d3f0f3072b3c8a4d45714a0
edcope <ed.cope@codethink.co.uk>
2025-11-01 22:43:47 +0100

    rust: removed fenix dep

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,9 +1,5 @@
 {
   inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,21 +8,33 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { flake-parts, nixpkgs, rust-overlay, ... } @ inputs:
+  outputs =
+    {
+      flake-parts,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
       ];
-      perSystem = { self', inputs', system, pkgs, ... }:
+      perSystem =
+        {
+          system,
+          pkgs,
+          ...
+        }:
         let
+          rust = pkgs.rust-bin.stable.latest.default;
+
+          # the correct rust-doc command is made, no need to modify this
           rust-doc = pkgs.writeShellApplication {
             name = "rust-doc";
             text = ''
-              xdg-open "${inputs'.fenix.packages.stable.rust-docs}/share/doc/rust/html/index.html"
+              xdg-open "${rust}/share/doc/rust/html/index.html"
             '';
           };
-
-          rust = pkgs.rust-bin.stable.latest.default;
         in
         {
           _module.args.pkgs = import nixpkgs {
